436b269ab0c953c04da8cb708d52d8e19e9fd780
Israel Gadelha <gdelha.israel@gmail.com>
2021-11-13 16:43:56 -0300

    Feat: Criado o READ FindAll com paginação

--- src/main/java/com/israelgda/clientesapi/service/ClientService.java
+++ src/main/java/com/israelgda/clientesapi/service/ClientService.java
@@ -6,6 +6,8 @@ import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,15 +18,22 @@ import com.israelgda.clientesapi.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> client = repository.findById(id);
-		ClientDTO result = new ClientDTO(client.orElseThrow(() -> new ResourceNotFoundException("Entity not found, id: " + id)));
+		ClientDTO result = new ClientDTO(
+				client.orElseThrow(() -> new ResourceNotFoundException("Entity not found, id: " + id)));
 		return result;
+	}
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(PageRequest pageable) {
+		Page<Client> list = repository.findAll(pageable);
+		return list.map(result -> new ClientDTO(result));
 	}
 
 	@Transactional
@@ -33,30 +42,29 @@ public class ClientService {
 		client = repository.save(client);
 		return new ClientDTO(client);
 	}
-	
+
 	@Transactional
 	public ClientDTO update(Long id, ClientDTO clientDTO) {
 		try {
 			Client newClient = repository.getOne(id);
 			repository.save(updateClient(newClient, clientDTO));
-			
+
 			return new ClientDTO(newClient);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Entity not found, id: " + id);
 		}
-		
+
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException("Entity not found, id: " + id);
 		}
-		
+
 	}
-	
-	
+
 	private Client updateClient(Client newClient, ClientDTO clientDTO) {
 		newClient.setName(clientDTO.getName());
 		newClient.setCpf(clientDTO.getCpf());
