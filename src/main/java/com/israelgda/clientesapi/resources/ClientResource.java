0dedc816ee23bcfdc801dff8badaad324418f40f
Israel Gadelha <gdelha.israel@gmail.com>
2021-11-13 16:28:42 -0300

    Feat: Criado o DELETE

--- src/main/java/com/israelgda/clientesapi/resources/ClientResource.java
+++ src/main/java/com/israelgda/clientesapi/resources/ClientResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,4 +48,9 @@ public class ClientResource {
 		return ResponseEntity.ok().body(newClient);
 	}
 
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
